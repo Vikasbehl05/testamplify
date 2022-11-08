2ee5df82e1dbf66ee26a9dd13e4ee3aec62f376d
Vikasbehl05 <88477915+Vikasbehl05@users.noreply.github.com>
2022-11-08 22:03:11 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import { ReactDOM } from "react";
+
+class Header extends React.Component {
+  render() {
+    return <h1>Hello React Good Package </h1>;
+  }
+}
+
+export default Header;
